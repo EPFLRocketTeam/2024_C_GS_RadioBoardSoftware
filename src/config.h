6635327769012ef057786a6e8092bddf5630ddfd
BuildTools <unconfigured@null.spigotmc.org>
2024-08-07 18:32:45 -0400

    Clean and Document

--- src/config.h
+++ src/config.h
@@ -1,20 +1,21 @@
-#define DEBUG 				false
+#define DEBUG false
 
-#define MODE_STANDALONE   true  // otherwise, connected to motherboard
+#define MODE_STANDALONE true
+#define SEND_TO_DB false
 
-#define SEND_TO_DB      false
-
-
-
+// In standalone mode, the board will always send data to the PC
 #if MODE_STANDALONE
 #define UART_PORT   		USBSerial
+
+// In motherboard mode, the Serial1 interface is used to connect the radioboard to the motherboard
 #else
 #define UART_PORT   		Serial1
 #endif
+
 #define UART_BAUD   		115200
 
 #define SERIAL_TO_PC        USBSerial
-#define SERIAL_TO_PC_BAUD   115200		// baudrate not considered as USB speed
+#define SERIAL_TO_PC_BAUD   115200
 
 // PIN/GPIO Definition on Radio Module ERT
 
@@ -28,47 +29,55 @@
 
 #define NEOPIXEL_PIN            18
 
+// Board is used to emit from ground control to the rocket and GSE
 #ifdef UPLINK
-  #define LORA_FREQ         UPLINK_FREQUENCY
-  #define LORA_POWER        UPLINK_POWER
-  #define LORA_BW           UPLINK_BW
-  #define LORA_SF           UPLINK_SF
-  #define LORA_CR           UPLINK_CR
-  #define LORA_PREAMBLE_LEN UPLINK_PREAMBLE_LEN
-  #define LORA_SYNC_WORD    UPLINK_SYNC_WORD
-  #define LORA_CRC          UPLINK_CRC
-  #define LORA_INVERSE_IQ   UPLINK_INVERSE_IQ
+	#define LORA_FREQ         UPLINK_FREQUENCY
+	#define LORA_POWER        UPLINK_POWER
+	#define LORA_BW           UPLINK_BW
+	#define LORA_SF           UPLINK_SF
+	#define LORA_CR           UPLINK_CR
+	#define LORA_PREAMBLE_LEN UPLINK_PREAMBLE_LEN
+	#define LORA_SYNC_WORD    UPLINK_SYNC_WORD
+	#define LORA_CRC          UPLINK_CRC
+	#define LORA_INVERSE_IQ   UPLINK_INVERSE_IQ
 
-  #define INITIAL_LED_COLOR 0
+	#define INITIAL_LED_COLOR 0
 
-  #define RADIOMODULE_NAME "UPLINK"
+	#define RADIOMODULE_NAME "UPLINK"
+
+// Board is used to receive data from the rocket
 #elif AV_DOWNLINK
-  #define LORA_FREQ         AV_DOWNLINK_FREQUENCY
-  #define LORA_POWER        AV_DOWNLINK_POWER
-  #define LORA_BW           AV_DOWNLINK_BW
-  #define LORA_SF           AV_DOWNLINK_SF
-  #define LORA_CR           5
-  #define LORA_PREAMBLE_LEN AV_DOWNLINK_PREAMBLE_LEN
-  #define LORA_SYNC_WORD    AV_DOWNLINK_SYNC_WORD
-  #define LORA_CRC          false //AV_DOWNLINK_CRC
-  #define LORA_INVERSE_IQ   AV_DOWNLINK_INVERSE_IQ
-  #define INITIAL_LED_COLOR 1
+	#define LORA_FREQ         AV_DOWNLINK_FREQUENCY
+	#define LORA_POWER        AV_DOWNLINK_POWER
+	#define LORA_BW           AV_DOWNLINK_BW
+	#define LORA_SF           AV_DOWNLINK_SF
+	#define LORA_CR           5
+	#define LORA_PREAMBLE_LEN AV_DOWNLINK_PREAMBLE_LEN
+	#define LORA_SYNC_WORD    AV_DOWNLINK_SYNC_WORD
+	#define LORA_CRC          false //AV_DOWNLINK_CRC
+	#define LORA_INVERSE_IQ   AV_DOWNLINK_INVERSE_IQ
+	#define INITIAL_LED_COLOR 1
 
-  #define RADIMODULE_NAME "AV_DOWNLINK_1"
+	#define RADIMODULE_NAME "AV_DOWNLINK_1"
+
+	// Board is used to receive data from the GSE
 #elif GSE_DOWNLINK
-  #define LORA_FREQ         GSE_DOWNLINK_FREQUENCY
-  #define LORA_POWER        GSE_DOWNLINK_POWER
-  #define LORA_BW           GSE_DOWNLINK_BW
-  #define LORA_SF           GSE_DOWNLINK_SF
-  #define LORA_CR           GSE_DOWNLINK_CR
-  #define LORA_PREAMBLE_LEN GSE_DOWNLINK_PREAMBLE_LEN
-  #define LORA_SYNC_WORD    GSE_DOWNLINK_SYNC_WORD
-  #define LORA_CRC          GSE_DOWNLINK_CRC
-  #define LORA_INVERSE_IQ   GSE_DOWNLINK_INVERSE_IQ
+	#define LORA_FREQ         GSE_DOWNLINK_FREQUENCY
+	#define LORA_POWER        GSE_DOWNLINK_POWER
+	#define LORA_BW           GSE_DOWNLINK_BW
+	#define LORA_SF           GSE_DOWNLINK_SF
+	#define LORA_CR           GSE_DOWNLINK_CR
+	#define LORA_PREAMBLE_LEN GSE_DOWNLINK_PREAMBLE_LEN
+	#define LORA_SYNC_WORD    GSE_DOWNLINK_SYNC_WORD
+	#define LORA_CRC          GSE_DOWNLINK_CRC
+	#define LORA_INVERSE_IQ   GSE_DOWNLINK_INVERSE_IQ
 
-  #define INITIAL_LED_COLOR 2
+	#define INITIAL_LED_COLOR 2
 
-  #define RADIOMODULE_NAME "GSE_DOWNLINK"
+	#define RADIOMODULE_NAME "GSE_DOWNLINK"
 #endif
 
 #define LORA_CURRENT_LIMIT 120
+
+#define GS_LAT 39.39482;
+#define GS_LON -8.29242;
--- src/main.cpp
+++ src/main.cpp
@@ -65,325 +65,284 @@ double compute_downrange(double rocket_lat, double rocket_lon);
 void plotPoints(av_downlink_t packet);
 #endif
 
-
 #endif
 
 #define LED_COLOR_TIME 100 // Color of the led will be changed for x ms each time a packet is received
-  static unsigned long lastPacketReceived = 0;
+static unsigned long lastPacketReceived = 0;
 
-  uint32_t colors[] = {
-      0xFF0000, // Red
-      0x00FF00, // Green
-      0x0000FF, // Blue
-      0x32A8A0, // Cyan
-      0xFFEA00, // Yellow
-      0xCF067C, // Purple
-      0xFF0800  // Orange
-  };
+uint32_t colors[] = {
+	0xFF0000, // Red
+	0x00FF00, // Green
+	0x0000FF, // Blue
+	0x32A8A0, // Cyan
+	0xFFEA00, // Yellow
+	0xCF067C, // Purple
+	0xFF0800  // Orange
+};
 
-  void handlePacketLoRa(int packetSize);
-  void handleLoRaCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len);
-  void handleUartCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len);
+void handlePacketLoRa(int packetSize);
+void handleLoRaCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len);
+void handleUartCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len);
 
-  Adafruit_NeoPixel led(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800); // 1 led
-  LoopbackStream LoRaRxBuffer(1024);
-  CapsuleStatic LoRaCapsule(handleLoRaCapsule);
-  CapsuleStatic UartCapsule(handleUartCapsule);
+Adafruit_NeoPixel led(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800); // 1 led
+LoopbackStream LoRaRxBuffer(1024);
+CapsuleStatic LoRaCapsule(handleLoRaCapsule);
+CapsuleStatic UartCapsule(handleUartCapsule);
 
-  void setup()
-  {
-      SERIAL_TO_PC.begin(SERIAL_TO_PC_BAUD);
+void setup()
+{
+	// Debug channel setup
+	SERIAL_TO_PC.begin(SERIAL_TO_PC_BAUD);
 
-      sleep(5);
-      SERIAL_TO_PC.println("Startup Started");
-      SERIAL_TO_PC.setTxTimeoutMs(0);
+	sleep(5);
+	SERIAL_TO_PC.println("Startup Started");
+	SERIAL_TO_PC.setTxTimeoutMs(0);
 
-      // put your setup code here, to run once:
-#if !MODE_STANDALONE
-      UART_PORT.begin(UART_BAUD, 134217756U, 6, 5); // This for radioboard
-      // UART_PORT.begin(UART_BAUD, 134217756U, 9, 46); // This for cmdIn
-#endif
+	// In standalone mode, UART_PORT = SERIAL_TO_PC. In motherboard mode, UART_PORT = Serial1
+	// We thus only need to initialize UART_PORT in motherboard mode
+	#if !MODE_STANDALONE
+	UART_PORT.begin(UART_BAUD, 134217756U, 6, 5); // This for radioboard
+	// UART_PORT.begin(UART_BAUD, 134217756U, 9, 46); // This for cmdIn
+	#endif
 
-      led.begin();
-      led.fill(colors[INITIAL_LED_COLOR]);
-      led.show();
+	// Light up LED with the color corresponding to the board's config
+	led.begin();
+	led.fill(colors[INITIAL_LED_COLOR]);
+	led.show();
 
-      SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
-      LoRa.setPins(LORA_CS, LORA_RST, LORA_INT0);
-      LoRa.setSPI(SPI);
+	// LoRa setup
+	SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+	LoRa.setPins(LORA_CS, LORA_RST, LORA_INT0);
+	LoRa.setSPI(SPI);
 
-      if (!LoRa.begin(LORA_FREQ))
-      {
-          if (DEBUG)
-          {
-              SERIAL_TO_PC.println("Starting LoRa failed!");
-          }
-      }
+	if (DEBUG && !LoRa.begin(LORA_FREQ)) SERIAL_TO_PC.println("Starting LoRa failed!");
 
-      // Set LoRa parameters
-      LoRa.setTxPower(LORA_POWER);
-      LoRa.setSpreadingFactor(LORA_SF);
-      LoRa.setSignalBandwidth(LORA_BW);
-      LoRa.setCodingRate4(LORA_CR);
-      LoRa.setPreambleLength(LORA_PREAMBLE_LEN);
-#if (LORA_CRC)
-      LoRa.enableCrc(); // not necessary to work with miaou, even if miaou enbale it...:-|
-#else
-    LoRa.disableCrc();
-#endif
-#if (LORA_INVERSE_IQ)
-      LoRa.enableInvertIQ();
-#else
-    LoRa.disableInvertIQ();
-#endif
-      LoRa.onReceive(handlePacketLoRa);
-      LoRa.receive();
-      LoRa.receive();
-      
+	// Set LoRa parameters
+	LoRa.setTxPower(LORA_POWER);
+	LoRa.setSpreadingFactor(LORA_SF);
+	LoRa.setSignalBandwidth(LORA_BW);
+	LoRa.setCodingRate4(LORA_CR);
+	LoRa.setPreambleLength(LORA_PREAMBLE_LEN);
 
+	#if (LORA_CRC)
+	LoRa.enableCrc(); // not necessary to work with miaou, even if miaou enbale it...:-|
+	#else
+	LoRa.disableCrc();
+	#endif
 
-    // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-#if SEND_TO_DB
-      setupInfluxDb();
-#endif
+	#if (LORA_INVERSE_IQ)
+	LoRa.enableInvertIQ();
+	#else
+	LoRa.disableInvertIQ();
+	#endif
+
+	LoRa.onReceive(handlePacketLoRa);
+	LoRa.receive();
+	LoRa.receive();
+	  
+	// WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	#if SEND_TO_DB
+	setupInfluxDb();
+	#endif
  
-    
-      SERIAL_TO_PC.println("Startup Finished");
-    
-
-
+	SERIAL_TO_PC.println("Startup Finished");
 }
 
+/*
 const uint16_t packetSizes[] = {8, 16, 32, 64, 128, 256}; // Packet sizes in bytes
 const int numPacketSizes = sizeof(packetSizes) / sizeof(packetSizes[0]);
+*/
 
 const uint8_t packetSize = 0xFF; // Packet size in bytes
 const unsigned long targetDataRate = 367000; // Target data rate in bits per second
 const unsigned long totalBitsToSend = 10000;
 
 void loop() {
-    
 
-  /*
-  av_downlink_t packet_debug;
-  // uint8_t p[80];
+	// Incoming data from the LoRa is processed.
+	// While this is config-independant, it won't do much if the LoRa is not supposed to act as Rx.
+	while (LoRaRxBuffer.available()) {
+		LoRaCapsule.decode(LoRaRxBuffer.read());
+  	}
 
-  packet_debug.gnss_lat = 12.0;
-  packet_debug.gnss_lon = 12.0;
-  packet_debug.gnss_alt = 12.0;
-  packet_debug.gnss_vertical_speed = 12.0;
-  packet_debug.av_state = 12.0;
-  packet_debug.chamber_pressure = 12.0;
-  packet_debug.LOX_pressure = 12.0;
-  packet_debug.av_state = 12;
+	// Incoming data from the UART_PORT is processed.
+	// In standalone mode, this data is sent by the computer. In motherboard mode, this data has been routed by the motherboard.
+	while (UART_PORT.available()) {
+		UartCapsule.decode(UART_PORT.read());
+  	}
 
-  uint8_t len = sizeof(packet_debug);
-  uint8_t packetToSend[len];
-
-  memcpy(packetToSend, &packet_debug, len);
-  // send it over serial
-  uint8_t* packetToSend2 = UartCapsule.encode(CAPSULE_ID::AV_TELEMETRY,packetToSend,len);
-  SERIAL_TO_PC.write(packetToSend2,UartCapsule.getCodedLen(len));
-
-  sleep(1);
-
-
-    */
-
-  while (LoRaRxBuffer.available()) {
-      // char x = LoRaRxBuffer.read();
-      // LoRaCapsule.decode(x);
-      // USBSerial.println(x);
-      // USBSerial.print("m");
-
-      LoRaCapsule.decode(LoRaRxBuffer.read());
-  }
-
-    while (UART_PORT.available()) {
-      UartCapsule.decode(UART_PORT.read());
-  }
-
-  if ((millis()-lastPacketReceived)>LED_COLOR_TIME) {
-      led.fill(colors[INITIAL_LED_COLOR]);
-      led.show();
-  }
-  /*
-  #if AV_DOWNLINK
-      av_downlink_t packet = {1380336199, 2, 3214, 1013, 25, 1000, 2000, 3000, 123.9091, 43.90123, 3000.0, 123.9091, 43.90123, 3000.0, 13, 12.902, 12.0, 12.0,2, 1, 1, 1, 1, 1, 1, 1};;
-      uint8_t p[80];
-      memcpy(p, &packet, 80);
-      SERIAL_TO_PC.println("send packet");
-      udp.broadcastTo(p, 80, 1235);
-  #endif
-  */
-  // static uint32_t t = 0;
-  // if (millis() - t > 4000) {
-  //     t = millis();
-  //     uint8_t x[] = {1,2,3,4,5,6,7};
-  //     uint8_t *packetToSend = LoRaCapsule.encode(0x12, x, sizeof(x));
-  //     LoRa.beginPacket();
-  //     LoRa.write(packetToSend, LoRaCapsule.getCodedLen(sizeof(x)));
-  //     LoRa.endPacket();
-  //     LoRa.receive();
-  //     delete[] packetToSend;
-  // }
+	// The visual clue is reset to the "config color".
+	if ((millis() - lastPacketReceived) > LED_COLOR_TIME) {
+		led.fill(colors[INITIAL_LED_COLOR]);
+		led.show();
+	}
 }
 
+// Handler for raw LoRa Rx data
 void handlePacketLoRa(int packetSize) {
-    SERIAL_TO_PC.println("Packet received");
-    SERIAL_TO_PC.println(packetSize);
-    for (int i = 0; i < packetSize; i++) {
-        LoRaRxBuffer.write(LoRa.read());
-    }
+	// Debug message
+	SERIAL_TO_PC.println("Packet received");
+	SERIAL_TO_PC.println(packetSize);
+
+	// Incoming data is stored in the LoRaRxBuffer for decoding by Cpasule
+	for (int i = 0; i < packetSize; i++) {
+		LoRaRxBuffer.write(LoRa.read());
+	}
 }
 
 void handleLoRaCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len) {
-    lastPacketReceived = millis();
-    uint32_t ledColor = colors[INITIAL_LED_COLOR+1];
-    led.fill(ledColor);
-    led.show();
-    
-    uint8_t* packetToSend = UartCapsule.encode(packetId,dataIn,len);
-    UART_PORT.write(packetToSend,UartCapsule.getCodedLen(len));
-    
-    delete[] packetToSend;
-    #if SEND_TO_DB
-        #if AV_DOWNLINK
-            av_downlink_t packet_debug;
-            // uint8_t p[80];
-            //av_downlink_t packet_debug;
-            memcpy(&packet_debug, dataIn, 65);
+	// Nice visual clue to show that the board has received a new packet.
+	lastPacketReceived = millis();
+	uint32_t ledColor = colors[INITIAL_LED_COLOR+1];
+	led.fill(ledColor);
+	led.show();
+	
+	// The radio boards do not do any processing besides LoRa/Capsule encoding/decoding.
+	// The packet thusly is routed through the UART_PORT.
+	uint8_t* packetToSend = UartCapsule.encode(packetId,dataIn,len);
+	UART_PORT.write(packetToSend,UartCapsule.getCodedLen(len));
+	delete[] packetToSend;
+	
+	// Should the database be enabled, any received packet is also pushed to the database.
+	#if SEND_TO_DB
+	#ifdef AV_DOWNLINK
+	av_downlink_t packet_debug;
+	// uint8_t p[80];
+	//av_downlink_t packet_debug;
+	memcpy(&packet_debug, dataIn, 65);
 
-            //SERIAL_TO_PC.println(packet_debug.gnss_alt);
-            // memcpy(p, &dataIn, 80);
-            SERIAL_TO_PC.println(packet_debug.gnss_alt);
-            udp.broadcastTo(dataIn, len, 1235);
-        #endif
-    #endif
-
-#if SEND_TO_DB
-    // InfluxDB stream
-    #if GSE_DOWNLINK
-        PacketGSE_downlink packetInflux;
-        memcpy(&packetInflux, dataIn, packetGSE_downlink_size);
-        plotPoints(packetInflux);
-    #endif
-    #if AV_DOWNLINK
-        av_downlink_t packetInflux;
-        memcpy(&packetInflux, dataIn, 65);
-        plotPoints(packetInflux);
-    #endif
-#endif
+	//SERIAL_TO_PC.println(packet_debug.gnss_alt);
+	// memcpy(p, &dataIn, 80);
+	SERIAL_TO_PC.println(packet_debug.gnss_alt);
+	udp.broadcastTo(dataIn, len, 1235);
+	#endif
+	#endif
+	
+	// Should the database be enabled, points are plotted.
+	#if SEND_TO_DB
+		// InfluxDB stream
+		#if GSE_DOWNLINK
+			PacketGSE_downlink packetInflux;
+			memcpy(&packetInflux, dataIn, packetGSE_downlink_size);
+			plotPoints(packetInflux);
+		#endif
+		#if AV_DOWNLINK
+			av_downlink_t packetInflux;
+			memcpy(&packetInflux, dataIn, 65);
+			plotPoints(packetInflux);
+		#endif
+	#endif
 }
 
 void handleUartCapsule(uint8_t packetId, uint8_t *dataIn, uint32_t len) {
-    lastPacketReceived = millis();
-    uint32_t ledColor = colors[INITIAL_LED_COLOR+1];
-    led.fill(ledColor);
-    led.show();
+	lastPacketReceived = millis();
+	uint32_t ledColor = colors[INITIAL_LED_COLOR+1];
+	led.fill(ledColor);
+	led.show();
 
-    uint8_t* packetToSend = LoRaCapsule.encode(packetId,dataIn,len);
-    LoRa.beginPacket();
-    LoRa.write(packetToSend,LoRaCapsule.getCodedLen(len));
-    LoRa.endPacket();
-    LoRa.receive();
-    LoRa.receive();
-    delete[] packetToSend;
+	uint8_t* packetToSend = LoRaCapsule.encode(packetId,dataIn,len);
+	LoRa.beginPacket();
+	LoRa.write(packetToSend,LoRaCapsule.getCodedLen(len));
+	LoRa.endPacket();
+	LoRa.receive();
+	LoRa.receive();
+	delete[] packetToSend;
 }
 
 #if SEND_TO_DB
-    void setupInfluxDb() {
-    // Connect WiFi
+void setupInfluxDb() {
+	// Connect WiFi
    
-    WiFi.mode(WIFI_STA);
-    wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
-    while (wifiMulti.run() != WL_CONNECTED) {
-        SERIAL_TO_PC.print(".");
-        delay(500);
-    }
+	WiFi.mode(WIFI_STA);
+	wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+	while (wifiMulti.run() != WL_CONNECTED) {
+		SERIAL_TO_PC.print(".");
+		delay(500);
+	}
 
-    SERIAL_TO_PC.println();
-    SERIAL_TO_PC.println("Wifi connected :-)");
+	SERIAL_TO_PC.println();
+	SERIAL_TO_PC.println("Wifi connected :-)");
 
-    // Set InfluxDB 1 authentication params
-    // client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
+	// Set InfluxDB 1 authentication params
+	// client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
 
-    // Add constant tags - only once
-    // GSETelemetry.addTag("device", "GSEReceiver");
-    
-    // Check server connection
-    if (client.validateConnection()) {
-        SERIAL_TO_PC.print("Connected to InfluxDB: ");
-        SERIAL_TO_PC.println(client.getServerUrl());
-    } else {
-        SERIAL_TO_PC.print("InfluxDB connection failed: ");
-        SERIAL_TO_PC.println(client.getLastErrorMessage());
-    }
-  }
-    #ifdef GSE_DOWNLINK
-    void plotPoints(PacketGSE_downlink packet) {
-    // Point data
-        GSETelemetry.addField("Tank Pressure", packet.tankPressure);
-        GSETelemetry.addField("Tank Temperature", packet.tankTemperature);
-        GSETelemetry.addField("Filling Pressure", packet.fillingPressure);
-        GSETelemetry.addField("loadcellRaw", packet.loadcellRaw);
-        GSETelemetry.addField("Fill N2O", packet.status.fillingN2O == ACTIVE ? 1.0:0.0);
-        GSETelemetry.addField("Disconnect Active", (packet.disconnectActive == ACTIVE ? 1.0:0.0));
-        GSETelemetry.addField("GSE Purge", (packet.status.vent == ACTIVE ? 1.0:0.0));
-        if (!client.writePoint(GSETelemetry)) {
-            Serial.print("InfluxDB write failed: ");
-            Serial.println(client.getLastErrorMessage());
-        }
-    
-    }
-    #endif
-    #ifdef AV_DOWNLINK
-    void plotPoints(av_downlink_t packet) {
-    // Point data
-        AVTelemetry.addField("Lat", packet.gnss_lat);
-        AVTelemetry.addField("Lon", packet.gnss_lon);
-        AVTelemetry.addField("Downrange", compute_downrange(packet.gnss_lat, packet.gnss_lon));
-        SERIAL_TO_PC.println("here we are");
-        AVTelemetry.addField("Alt", packet.gnss_alt);
-        AVTelemetry.addField("Vertical Speed", packet.gnss_vertical_speed);
-        AVTelemetry.addField("Tank Temperature", packet.tank_temp);
-        AVTelemetry.addField("Chamber Pressure", packet.chamber_pressure);
-        AVTelemetry.addField("N2O Pressure", packet.N2O_pressure);
-        AVTelemetry.addField("AV State", packet.av_state);
-        SERIAL_TO_PC.println("here we are 2");
-        AVTelemetry.addField("Valve Pressurize", packet.engine_state.pressurize== 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Valve Purge", packet.engine_state.purge == 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Valve Reserve", packet.engine_state.reserve== 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Servo Fuel", packet.engine_state.servo_fuel== 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Servo N2O", packet.engine_state.servo_N2O== 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Vent Fuel", packet.engine_state.vent_fuel== 1 ? 1.0 : 0.0);
-        AVTelemetry.addField("Vent N2O", packet.engine_state.vent_N2O == 1 ? 1.0 : 0.0);
-        SERIAL_TO_PC.println("here we are 3");
-        if (!client.writePoint(AVTelemetry)) {
-            Serial.print("InfluxDB write failed: ");
-            Serial.println(client.getLastErrorMessage());
-        }
-        SERIAL_TO_PC.print("InfluxDB write success");
-    } 
-    #endif
-
-    
-
+	// Add constant tags - only once
+	// GSETelemetry.addTag("device", "GSEReceiver");
+	
+	// Check server connection
+	if (client.validateConnection()) {
+		SERIAL_TO_PC.print("Connected to InfluxDB: ");
+		SERIAL_TO_PC.println(client.getServerUrl());
+	} else {
+		SERIAL_TO_PC.print("InfluxDB connection failed: ");
+		SERIAL_TO_PC.println(client.getLastErrorMessage());
+	}
+}
+#ifdef GSE_DOWNLINK
+void plotPoints(PacketGSE_downlink packet) {
+	// Point data
+	GSETelemetry.addField("Tank Pressure", packet.tankPressure);
+	GSETelemetry.addField("Tank Temperature", packet.tankTemperature);
+	GSETelemetry.addField("Filling Pressure", packet.fillingPressure);
+	GSETelemetry.addField("loadcellRaw", packet.loadcellRaw);
+	GSETelemetry.addField("Fill N2O", packet.status.fillingN2O == ACTIVE ? 1.0:0.0);
+	GSETelemetry.addField("Disconnect Active", (packet.disconnectActive == ACTIVE ? 1.0:0.0));
+	GSETelemetry.addField("GSE Purge", (packet.status.vent == ACTIVE ? 1.0:0.0));
+	if (!client.writePoint(GSETelemetry)) {
+		Serial.print("InfluxDB write failed: ");
+		Serial.println(client.getLastErrorMessage());
+	}
+	
+}
+#endif
+#ifdef AV_DOWNLINK
+void plotPoints(av_downlink_t packet) {
+// Point data
+	AVTelemetry.addField("Lat", packet.gnss_lat);
+	AVTelemetry.addField("Lon", packet.gnss_lon);
+	AVTelemetry.addField("Downrange", compute_downrange(packet.gnss_lat, packet.gnss_lon));
+	SERIAL_TO_PC.println("here we are");
+	AVTelemetry.addField("Alt", packet.gnss_alt);
+	AVTelemetry.addField("Vertical Speed", packet.gnss_vertical_speed);
+	AVTelemetry.addField("Tank Temperature", packet.tank_temp);
+	AVTelemetry.addField("Chamber Pressure", packet.chamber_pressure);
+	AVTelemetry.addField("N2O Pressure", packet.N2O_pressure);
+	AVTelemetry.addField("AV State", packet.av_state);
+	SERIAL_TO_PC.println("here we are 2");
+	AVTelemetry.addField("Valve Pressurize", packet.engine_state.pressurize== 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Valve Purge", packet.engine_state.purge == 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Valve Reserve", packet.engine_state.reserve== 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Servo Fuel", packet.engine_state.servo_fuel== 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Servo N2O", packet.engine_state.servo_N2O== 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Vent Fuel", packet.engine_state.vent_fuel== 1 ? 1.0 : 0.0);
+	AVTelemetry.addField("Vent N2O", packet.engine_state.vent_N2O == 1 ? 1.0 : 0.0);
+	SERIAL_TO_PC.println("here we are 3");
+	if (!client.writePoint(AVTelemetry)) {
+		Serial.print("InfluxDB write failed: ");
+		Serial.println(client.getLastErrorMessage());
+	}
+	SERIAL_TO_PC.print("InfluxDB write success");
+} 
+#endif
 #endif
 double compute_downrange(double rocket_lat, double rocket_lon) {
-        // Hardcoded Portugal GS location
-        float gs_lat = 39.39482;
-        float gs_lon = -8.29242;
-        // haversineDistance
-        double earthRadius = 6371000.0;
-        gs_lat = gs_lat * M_PI / 180.0;
-        gs_lon = gs_lon * M_PI / 180.0;
-        rocket_lat = rocket_lat * M_PI / 180.0;
-        rocket_lon = rocket_lon * M_PI / 180.0;
-        double dlat = rocket_lat - gs_lat;
-        double dlon = rocket_lon - gs_lon;
-        double a = sin(dlat/2) * sin(dlat/2) + cos(gs_lat) * cos(rocket_lat) * sin(dlon/2) * sin(dlon/2);
-        double c = 2 * atan2(sqrt(a), sqrt(1-a));
-        double distance = earthRadius * c;
-        return distance;
-    }
+		float gs_lat = GS_LAT;
+		float gs_lon = GS_LON;
+
+		// haversineDistance
+		double earthRadius = 6371000.0;
+		gs_lat = gs_lat * M_PI / 180.0;
+		gs_lon = gs_lon * M_PI / 180.0;
+
+		rocket_lat = rocket_lat * M_PI / 180.0;
+		rocket_lon = rocket_lon * M_PI / 180.0;
+
+		double dlat = rocket_lat - gs_lat;
+		double dlon = rocket_lon - gs_lon;
+
+		double a = sin(dlat/2) * sin(dlat/2) + cos(gs_lat) * cos(rocket_lat) * sin(dlon/2) * sin(dlon/2);
+		double c = 2 * atan2(sqrt(a), sqrt(1-a));
+		double distance = earthRadius * c;
+		
+		return distance;
+	}
